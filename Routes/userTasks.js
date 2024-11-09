83f81e01cafe9365910106ecf9e16181cf25a0ee
Akshat Tiwari <akshattiwari@Akshats-MacBook-Air.local>
2024-11-09 17:49:03 +0530

    commit

--- Routes/userTasks.js
+++ Routes/userTasks.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
   submitTasks
-} = require('../controllers/scheduleController');
+} = require('../Controllers/scheduleController');
 const authenticate = require('../Middleware/authenticate');
 
 router.post('/',authenticate, submitTasks);
