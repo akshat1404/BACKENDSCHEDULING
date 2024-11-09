907b53a245eccd53353c03e630b03f44bdf177cb
Akshat Tiwari <akshattiwari@Akshats-MacBook-Air.local>
2024-11-09 18:34:04 +0530

    logs

--- Server.js
+++ Server.js
@@ -17,8 +17,10 @@ const port = process.env.PORT || 800;
 
 app.use(cors());
 app.use(express.json()); 
- 
-mongoose.connect(process.env.MONGO_URI, { socketTimeoutMS: 30000, connectTimeoutMS: 30000 })
+
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
