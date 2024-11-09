c5cdb30e6a6a9761161016b06465e551fe1ec99f
Akshat Tiwari <akshattiwari@Akshats-MacBook-Air.local>
2024-11-09 18:53:30 +0530

    afds

--- Server.js
+++ Server.js
@@ -18,9 +18,7 @@ const port = process.env.PORT || 800;
 app.use(cors());
 app.use(express.json()); 
 
-console.log(process.env.MONGO_URI);
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://Akshat_Tiwari_1404:Akshat_1404@scheduling.2wuji.mongodb.net/schedulesDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
